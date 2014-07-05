fcbf298380f59f85a8babe38eaedb202f1100a3b
Allen Li <allen@allen.li>
2014-07-04 19:04:49 -0700

    have a config

--- run.py
+++ run.py
@@ -1,2 +1,46 @@
+import os
 from subprocess import call
-call("./btsync")
+call(["mkdir -p /tmp/data"])
+
+secret = str(os.environ.get('SYNC_SECRET'))
+
+CONFIG_SKELETON = """
+{
+  "device_name": "mirror",
+  "listening_port" : 0,                       // 0 - randomize port
+  "storage_path" : "/home/user/.sync",
+  "check_for_updates" : false, 
+  "use_upnp" : false, 
+  "download_limit" : 0,
+  "upload_limit" : 0,
+  "webui" : // this is disabled
+    {
+      //"listen" : "0.0.0.0:8888",
+      //"login" : "admin",
+      //"password" : "password"
+    }
+  ,
+  "shared_folders" :
+  [
+    {
+      "secret" : "%s",
+      "dir" : "/tmp/data", 
+      "use_relay_server" : true,
+      "use_tracker" : true,
+      "use_dht" : true,
+      "search_lan" : false,
+      "use_sync_trash" : true,
+      "overwrite_changes" : true,
+      "known_hosts" :
+      [
+        //"192.168.1.2:44444"
+      ]
+    }
+  ]
+}
+""" % secret
+
+f = open('config', 'w')
+f.write(CONFIG_SKELETON)
+
+call(["./btsync", "--config", "./config"])
